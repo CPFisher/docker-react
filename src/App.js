d6944c9f0bae47a518faf4d176079509d63728ea
Craig Fisher <craig.p.fisher@gmail.com>
2023-05-02 23:12:01 -0500

    Updated text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello there! Testing it out<br/>
+         Craig is testing this out!
         </p>
         <a
           className="App-link"
